9c6d49e992c52fc4b2aa4912708bab7ab104fb59
Shrivrat Mani <shrivratmani@gmail.com>
2026-07-08 11:27:02 +0530

    System roles and Temperature Implementation

---
+++ week_1/day_3/sys_tem.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+my_api_key = os.environ.get("GOQ_API_KEY")
+
+client = Groq(api_key = my_api_key)
+
+messages = []
+
+model = "llama-3.3-70b-versatile"
+ ## defining system role by giving instruction
+messages.append({
+    "role":"system",
+    "content": "you are an expert in giving brand names in one word that brings sales for the product."
+})
+
+messages.append({
+    "role": "user",
+    "content": "give me only one brand name for my food delivery app"
+})
+
+## default temperature = 0
+response = client.chat.completions.create(
+    model = model,
+    messages=messages,
+    temperature  = 2
+)
+
+print(response.choices[0].message.content)
+
+
+# response at temperatue = 0 => Tasty(safe => not creative)
+# response at temperatue = 1 => Deliko(creative)
+# response at temperatue = 2 => Flavro(very creative)
+
+
+#######################
+# system => assign post to llm i.e, you are junior dev, senior dev
+# temperature => give diffrent personality to llm, dont be creative(0) or use creativity(2), range is [0,2]
